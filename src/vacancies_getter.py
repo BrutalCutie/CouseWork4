61fefde0114b5f573a0467c551f9df1649c20067
tiken14 <tiken14@live.com>
2024-07-23 20:27:36 +0500

    Вторая часть работы над работой с классом парсером вакансий с hh.ru

--- src/vacancies_getter.py
+++ src/vacancies_getter.py
@@ -1,9 +1,8 @@
 import json
 from abc import ABC, abstractmethod
 import requests
-import os
 
-from config import DATA_PATH, AREAS_PATH
+from config import AREAS_PATH
 from src.vacancy import Vacancy
 
 
@@ -19,7 +18,7 @@ class HHJobs(MainParser):
     def __init__(self, city: str = "", search_field: str = "", salary: str = '', top: int = 0):
         self.url = 'https://api.hh.ru/vacancies'
         self.vacancies = []
-        self.params = {'text': '', 'page': 0, 'per_page': 100}
+        self.params = {'text': '', 'page': 0, 'per_page': 100, "only_with_salary": True}
 
         self.city = city
         self.search_field = search_field
@@ -28,6 +27,7 @@ class HHJobs(MainParser):
         self.salary_lower = 0
         self.salary_upper = 0
         self.salary_range = False
+        self.total_vacs = 0
 
         if self.salary.isdigit():
             self.params['salary'] = salary
@@ -45,7 +45,7 @@ class HHJobs(MainParser):
 
             self.params['salary'] = self.salary_lower
 
-    def load_vacancies(self):
+    def load_vacancies(self) -> None:
         while True:
 
             response = requests.get(self.url, params=self.params)
@@ -57,9 +57,38 @@ class HHJobs(MainParser):
             if not response_data.get('pages') or self.params['page'] == response_data['pages']:
                 break
             else:
+                self.total_vacs += len(response_data['items'])
                 self.params['page'] += 1
 
+        self.filter_by_salary()
 
+    def filter_by_salary(self) -> None:
+
+        self.vacancies = sorted(self.vacancies, key=lambda _: _['salary']['from'] or _['salary']['to'], reverse=True)
+
+        if self.salary_range:
+            tmp = []
+
+            for vacancy in self.vacancies:
+
+                salary_target = vacancy['salary']['from'] or vacancy['salary']['to']
+                if self.salary_lower < salary_target < self.salary_upper:
+                    tmp.append(vacancy)
+            self.vacancies = tmp
+
+    def as_vacancy_class(self) -> list[Vacancy]:
+        vac_classes_list = []
+        top_stop = self.top_vac != 0
+
+        for index, vac in enumerate(self.vacancies, 1):
+            vac_classes_list.append(Vacancy(vacancy_data=vac))
+
+            if top_stop and index == self.top_vac:
+                break
+
+        return vac_classes_list
+
+    def get_raw_data(self) -> list[dict]: return self.vacancies
 
     @staticmethod
     def get_area_id(city: str) -> int | None:
@@ -72,8 +101,15 @@ class HHJobs(MainParser):
                 if city.lower() == cities['name'].lower():
                     return cities['id']
         else:
-            return None
+            raise ValueError("Город не найден")
+
 
 if __name__ == '__main__':
 
-    getter = HHJobs()
+    getter = HHJobs('уфа', "120000")
+    getter.load_vacancies()
+
+    # print(getter)
+    for i in getter.as_vacancy_class():
+        print(i)
+    print(f"Всего найдено {getter.total_vacs} Вакансий")
